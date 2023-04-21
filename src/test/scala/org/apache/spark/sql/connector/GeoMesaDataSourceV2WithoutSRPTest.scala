7bd8e2ac18d87eeec3e9b9a6410baf21bf61576f
yj <yeojeong.yun89@gmail.com>
2023-04-21 18:16:45 +0900

    테스트코드 수정

--- src/test/scala/org/apache/spark/sql/connector/GeoMesaDataSourceV2WithoutSRPTest.scala
+++ src/test/scala/org/apache/spark/sql/connector/GeoMesaDataSourceV2WithoutSRPTest.scala
@@ -2,6 +2,10 @@ package org.apache.spark.sql.connector
 
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import org.geotools.data.{DataStore, DataStoreFinder}
+import org.locationtech.geomesa.spark.jts.udf.GeometricConstructorFunctions.{ST_PointFromText, constructorNames}
+import org.locationtech.geomesa.spark.jts.util.SQLFunctionHelper.nullableUDF
+import org.locationtech.geomesa.spark.jts.util.WKTUtils
+import org.locationtech.jts.geom.{GeometryCollection, Point}
 
 import java.util.{Map => JMap}
 import scala.collection.JavaConverters.mapAsJavaMapConverter
@@ -56,12 +60,34 @@ object GeoMesaDataSourceV2WithoutSRPTest {
     // 테스트2: Spark SQL insert
     spark.sql("select * from test2").show()
 
+    spark.sql("select * from test2").printSchema()
+
+    val ST_GeometryCollectionFromText: String => GeometryCollection = nullableUDF(text => WKTUtils.read(text).asInstanceOf[GeometryCollection])
+    spark.sqlContext.udf.register("ST_GeometryCollectionFromText", ST_GeometryCollectionFromText)
+
+
     spark.sql(s"""insert into test2
                  | select '5' as __fid__,
-                 | 'yjyj' as arrest,
-                 | 123123 as case_number,
-                 | current_timestamp() as dtg,
-                 | st_point (10.0, 20.0) as geom""".stripMargin)
+                 | 5 as fid,
+                 | 'yjyj' as name,
+                 | 123123 as double,
+                 | 123123 as long,
+                 | 1.2 as float,
+                 | true as bool,
+                 | current_timestamp() as date,
+                 | current_timestamp() as timestamp,
+                 | ST_PointFromText ('POINT (30 10)') as point,
+                 | ST_LineFromText ('LINESTRING (30 10, 10 30, 40 40)') as linestring,
+                 | ST_PolygonFromText ('POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))') as polygon,
+                 | ST_MPointFromText ('MULTIPOINT ((10 40), (40 30), (20 20), (30 10))') as multipoint,
+                 | ST_MLineFromText ('MULTILINESTRING ((10 10, 20 20, 10 40), (40 40, 30 30, 40 20, 30 10))') as multilinestring,
+                 | ST_MPolyFromText ('MULTIPOLYGON (((30 20, 45 40, 10 40, 30 20)),((15 5, 40 10, 10 20, 5 10, 15 5)))') as multipolygon,
+                 | ST_GeometryCollectionFromText ('GEOMETRYCOLLECTION (POINT (40 10),LINESTRING (10 10, 20 20, 10 40),POLYGON ((40 40, 20 45, 45 30, 40 40)))') as geometrycollection,
+                 | ST_GeomFromWKT ('POINT (30 10)') as geometry,
+                 | array('a','b','c') as liststring,
+                 | MAP(1, 'hehe', 2, 'hehehe', 3, 'hehehehe') as mapstring,
+                 | binary(12345) as bytes
+                 | """.stripMargin)
 
     spark.sql("select * from test2").show()
 
